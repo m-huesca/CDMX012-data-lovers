7a3e4dee1fd1a58492e70a6913a5d3d2d2f2cc4f
Mariana <mariana.hb95@gmail.com>
2022-02-21 07:48:29 -0800

    función de buscar lista

--- src/data.js
+++ src/data.js
@@ -1,5 +1,7 @@
 // estas funciones son de ejemplo
 import data from './data/pokemon/pokemon.js';
+//import pokemonDatabase from './src/main.js';
+//console.log(pokemonDatabase);
 /* export const filter = {
 
   filterData: function (searchBy){
@@ -17,16 +19,26 @@ for (var i=0 ; i < data.pokemon.length ; i++)
 }
 */
 
+// let result = pokemons.filter(pokemon => searchPoke(pokemon, given_pokemon))
 
-export const filterData = (searchBy) => {
-  var results = [];
-  var searchField = data.pokemon.name;
-  for (var i=0 ; i < data.pokemon.length ; i++)
-  {
-      if (data.pokemon[i][searchField] == searchBy) {
-          results.push(data.pokemon[i]);
-      }
-    }
-    return console.log(results);
-  };
+// let stringResult = JSON.stringify(result, null, 2);
+
+// JSON.stringify(result, null, 2)
+
+export let searchPokemon = function(pokemon_database, pokemon_name) {
+
+  return pokemon_database.filter(pokemon => pokemon.name === pokemon_name || pokemon.num === pokemon_name);
+}
+
+// export const filterData = (searchBy) => {
+//   var results = [];
+//   var searchField = data.pokemon.name;
+//   for (var i=0 ; i < data.pokemon.length ; i++)
+//   {
+//       if (data.pokemon[i][searchField] == searchBy) {
+//           results.push(data.pokemon[i]);
+//       }
+//     }
+//     return console.log(results);
+//   };
  
--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import {filterData} from './data.js';
+import {searchPokemon} from './data.js';
 // import data from './data/lol/lol.js';
 import data from './data/pokemon/pokemon.js';
 // import data from './data/rickandmorty/rickandmorty.js';
@@ -7,17 +7,20 @@ import data from './data/pokemon/pokemon.js';
 //var obj = JSON.parse(JSON.stringify(data));
 //document.getElementById("pokemonDisplay").innerHTML = obj.pokemon[0].num + " " + obj.pokemon[0].name + " " + obj.pokemon[0].type;
 
+let pokemon_database = data.pokemon;
+
 //funcion para template literals de pokemon cards
 function pokemonCard(pokemon){
     return `
     <div class="pokemonCard">
-    <img class="cardImage" src="${Object.values(pokemon.img).join("")}">
+     <img class="cardImage" src="${Object.values(pokemon.img).join("")}">
     <label class="cardNum"> ${Object.values(pokemon.num).join("")} </label>
    <p class="cardName"> ${Object.values(pokemon.name).join("")} </p>
    <label class="cardType"> ${Object.values(pokemon.type).join(" , ")} </label>
     </div>
     `
 }
+
 // display pokemon html + json
 document.getElementById("cardContainer").innerHTML = `
     ${data.pokemon.map(pokemonCard).join("")}
@@ -26,8 +29,23 @@ document.getElementById("cardContainer").innerHTML = `
 
 document.getElementById("searchButton").addEventListener("click", function(){
     let searchBy = document.getElementById("searchName").value;
-  document.getElementById("searchResults").value = filterData(searchBy);
+   let pokemon = searchPokemon(pokemon_database,searchBy);
+    // console.log(JSON.stringify(pokemon, null, 2))
+    
+   document.getElementById("searchResult").innerHTML = `
+   <div class="searchResult">
+    ${pokemon.map(pokemonCard).join("")}
+    </div>
+   `;
   });
+
+// function printSearch(pokemon) {
+//     document.getElementById("searchResult").innerHTML = `
+//     ${pokemon.map(pokemonCard).join("")}
+// `;
+// }
+  
+
 
 //console.log(data);
  //<img class="pokemonImage" src="${data.pokemon.img}>
